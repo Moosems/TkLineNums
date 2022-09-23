be40d020cf8cd59b994f043127f40703c0403dba
Moosems <95927277+Moosems@users.noreply.github.com>
2022-09-23 13:36:12 -0400

    1.3.5

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
  
 TkLineNums is a simple line numbering widget for Tkinter. It supports ttk themes through the set_to_ttk_style method.
 
-![img](/images/TkLineNumsPhoto.png)
+![img](https://github.com/Moosems/TkLineNums/raw/main/images/TkLineNumsPhoto.png)
--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from distutils.core import setup
 
+with open("README.md", "r") as file:
+    long_description = file.read()
+
 setup(
     name="tklinenums",
-    version="1.3",
+    version="1.3.5",
     description="A simple Tkinter widget for displaying line numbers",
-    readme="README.md",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Moosems",
     author_email="moosems.j@gmail.com",
     url="https://github.com/Moosems/TkLineNums",
