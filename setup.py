5bb96362a9deaec438dd053ad5e7c21bc4f08053
Moosems <95927277+Moosems@users.noreply.github.com>
2022-09-23 09:59:48 -0400

    1.2

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(name="tklinenums",
-    version="1.1",
+    version="1.2",
     description="A simple Tkinter widget for displaying line numbers",
     author="Moosems",
     author_email="moosems.j@gmail.com",
     url="https://github.com/Moosems/TkLineNums",
-    packages=[],
+    packages=["tklinenums"],
 )
