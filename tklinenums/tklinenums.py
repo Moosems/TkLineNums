5752feaaace368a1ff8a5d74f58aa1655135300a
Moosems <95927277+Moosems@users.noreply.github.com>
2022-10-17 13:41:10 -0400

    Add click scrolling
    Added basic clicking and scrolling features to the linenumbers. Need help with selection-scrolling (click-scrolling) as it is kinda buggy. You will find it on line 147-158 if you want to help

--- tklinenums/tklinenums.py
+++ tklinenums/tklinenums.py
@@ -7,25 +7,81 @@ from tkinter.font import Font
 system = str(platform.system())
 
 
+class TkLineNumError(Exception):
+    ...
+
+
 class TkLineNumbers(Canvas):
+    """
+    Creates a line number widget for a text widget. Options are the same as a tkinter Canvas widget and add the following:
+        * -justify (str): The justification of the line numbers. Can be "left", "right", or "center". Default is "left".
+        * -font (tuple | Font | str | None): The font of the line numbers. Default is the text widget's font.
+        * -editor (Text): The text widget to attach the line numbers to. (Required) (Second argument after master)
+
+    Methods to be used outside externally:
+        * .redraw(): Redraws the widget
+        * .resize(): Calculates the required width of the widget and resizes it according to the text widget's font and the number of lines
+        * .set_to_ttk_style(): Sets the foreground and background colors to the ttk style of the text widget
+        * .reload_font(): Reloads the font of the widget (Requires font as argument)
+    """
+
     def __init__(
-        self, master: Misc, editor: Text, font: tuple | Font, justify: str = "left"
+        self,
+        master: Misc,
+        editor: Text,
+        font: tuple | Font | str | None = None,
+        justify: str = "left",
+        *args,
+        **kwargs,
     ) -> None:
+        """Initializes the widget -- Internal use only"""
         self.textwidget = editor
         self.master = master
         self.justify = justify
         Canvas.__init__(
-            self, master, width=40, highlightthickness=0, borderwidth=2, relief="ridge"
+            self,
+            master,
+            width=40 if kwargs.get("width") is None else kwargs.get("width"),
+            highlightthickness=0
+            if kwargs.get("highlightthickness") is None
+            else kwargs.get("highlightthickness"),
+            borderwidth=2
+            if kwargs.get("borderwidth") is None
+            else kwargs.get("borderwidth"),
+            relief="ridge" if kwargs.get("relief") is None else kwargs.get("relief"),
+            *args,
+            **kwargs,
         )
         if isinstance(font, Font):
             self.font = font
-        else:
+        elif isinstance(font, tuple):
             self.font = Font(family=font[0], size=font[1])
+        elif isinstance(font, str):
+            self.font = Font(name=font, exists=True)
+        else:
+            self.font = Font(
+                family=" ".join(
+                    (
+                        font := self.textwidget["font"]
+                        .replace("{", "")
+                        .replace("}", "")
+                        .split(" ")
+                    )[:-1]
+                ),
+                size=font[-1],
+            )
         self.set_to_ttk_style()
 
-        self.bind("<MouseWheel>", self.mouse_scroll)
+        self.bind("<MouseWheel>", self.mouse_scroll, add=True)
+        self.bind("<Button-1>", self.click_see, add=True)
+        self.bind("<ButtonRelease-1>", self.unclick, add=True)
+        self.bind("<Double-Button-1>", self.double_click, add=True)
+        self.bind("<Button1-Motion>", self.drag, add=True)
+
+        self.click_pos = None
 
     def redraw(self, *args) -> None:
+        """Redraws the widget"""
         self.resize()
         self.delete("all")
         self.unbind_all("Button-1")
@@ -50,33 +106,128 @@ class TkLineNumbers(Canvas):
             self.tag_bind(num, "<Button-1>", self.click_see)
 
     def mouse_scroll(self, event: Event) -> None:
+        """Scrolls the text widget when the mouse wheel is scrolled -- Internal use only"""
         if system == "Darwin":
-            self.textwidget.yview_scroll(event.delta, "units")
-            self.redraw()
+            self.textwidget.yview_scroll(-1 * event.delta, "units")
         else:
             self.textwidget.yview_scroll(int(-1 * (event.delta / 120)), "units")
-            self.redraw()
+        self.redraw()
 
-    def click_see(self, *args) -> None:
+    def click_see(self, event: Event) -> None:
+        """When clicking on a line number it scrolls to that line -- Internal use only"""
+        # Is useless currently, need to figure out how to get the line number from the click to be centered in the text widget
+        # See vscode for example.
+        self.textwidget.tag_remove("sel", "1.0", "end")
         self.textwidget.mark_set(
-            "insert", self.textwidget.index(f"@{args[0].x},{args[0].y}")
+            "insert",
+            f"{self.textwidget.index(f'@{event.x},{event.y}').split('.')[0]}.0",
         )
+        self.textwidget.see("insert")
+        self.click_pos = self.textwidget.index("insert")
+
+    def unclick(self, event: Event) -> None:
+        """When the mouse button is released it removes the selection -- Internal use only"""
+        self.click_pos = None
+
+    def double_click(self, event: Event) -> None:
+        """Selects the line when double clicked -- Internal use only"""
+        self.textwidget.tag_remove("sel", "1.0", "end")
+        self.textwidget.tag_add("sel", "insert", "insert + 1 line")
+
+    def drag(self, event: Event) -> None:
+        """When click dragging it selects the text -- Internal use only"""
+        if self.click_pos is None:
+            return
+        start, end = self.textwidget.index("insert"), self.click_pos
+        if self.textwidget.compare("insert", ">", self.click_pos):
+            start, end = end, start
+        self.textwidget.mark_set("insert", f"@0,{event.y}")
+        self.textwidget.tag_remove("sel", "1.0", "end")
+        self.textwidget.tag_add("sel", start, end)
+        """ # Has a few bugs that need to be fixed -- Help wanted
+        first_line, last_line = int(self.textwidget.index("@0,0").split(".")[0]), int(
+            self.textwidget.index(f"@0,{self.textwidget.winfo_height()}").split(".")[0]
+        )
+        if (end := int(self.textwidget.index(end).split(".")[0])) == last_line:
+            self.textwidget.see(f"{last_line+1}.0")
+        elif (
+            start := int(self.textwidget.index(start).split(".")[0])
+        ) == first_line:  # Scrolling back up doesn't work
+            self.textwidget.see(f"{first_line-1}.0")
+        print(end, last_line, start, first_line)
+        """
+        self.redraw()
+
 
     def resize(self) -> None:
+        """Resizes the widget to fit the text widget"""
         end = self.textwidget.index("end").split(".")[0]
         self.config(width=self.font.measure(" 1234 ")) if int(
             end
         ) <= 1000 else self.config(width=self.font.measure(f" {end} "))
 
     def set_to_ttk_style(self) -> None:
-        ttk_bg = self.tk.eval("ttk::style lookup TLineNumbers -background")
+        """Sets the widget to the ttk style"""
+        self["bg"] = self.tk.eval("ttk::style lookup TLineNumbers -background")
         self.foreground = self.tk.eval("ttk::style lookup TLineNumbers -foreground")
-        self["bg"] = ttk_bg
 
-    def reload(self, font: tuple | Font) -> None:
+    def reload(self, font: tuple | Font | str = "TkFixedFont") -> None:
+        """Reloads the widget with a new font"""
         if isinstance(font, Font):
             self.font = font
-        else:
+        elif isinstance(font, tuple):
             self.font = Font(family=font[0], size=font[1])
+        elif isinstance(font, str):
+            self.font = Font(name=font, exists=True)
+        else:
+            self.font = Font(
+                family=" ".join(
+                    (
+                        font := self.textwidget["font"]
+                        .replace("{", "")
+                        .replace("}", "")
+                        .split(" ")
+                    )[:-1]
+                ),
+                size=font[-1],
+            )
         self.set_to_ttk_style()
         self.redraw()
+
+
+if __name__ == "__main__":
+    from tkinter import Text, Tk
+    import platform
+    from tkinter.font import Font
+    from tkinter.ttk import Style
+
+    system = str(platform.system())
+
+    if system == "Darwin":
+        contmand = "Command"
+    else:
+        contmand = "Control"
+
+    root = Tk()
+
+    style = Style()
+    style.configure("TLineNumbers", background="#ffffff", foreground="#2197db")
+
+    text = Text(root, wrap="char", font=("Courier New bold", 15))
+    text.pack(side="right")
+    text.focus()
+
+    for i in range(50):
+        text.insert("end", f"Line {i+1}\n")
+
+    linenums = TkLineNumbers(root, text)
+    linenums.pack(fill="y", side="left", expand=True)
+
+    text.bind("<Key>", lambda event: root.after_idle(linenums.redraw), add=True)
+    text.bind(f"<BackSpace>", lambda event: root.after_idle(linenums.redraw), add=True)
+    text.bind(
+        f"<{contmand}-v>", lambda event: root.after_idle(linenums.redraw), add=True
+    )
+    text["yscrollcommand"] = linenums.redraw
+
+    root.mainloop()
